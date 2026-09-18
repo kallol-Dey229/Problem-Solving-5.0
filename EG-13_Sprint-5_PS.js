8b2b62068e124801a689e3636a7f32c63e71c2b5
Kallol Dey <kalloldey067@gmail.com>
2026-09-18 23:41:41 +0600

    first commit

---
+++ EG-13_Sprint-5_PS.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+    // your code goes here
+};
+
+
+console.log(checkSubarraySum([1, 1, 2]));
