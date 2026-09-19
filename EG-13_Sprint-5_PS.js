fc82cad9437be78191e023195859ab99e4057f29
Kallol Dey <kalloldey067@gmail.com>
2026-09-20 00:05:16 +0600

    problem 2 solved

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -5,7 +5,7 @@
  * @param {number} k
  * @return {boolean}
  */
-var removeDuplicates = function(nums) {
+var removeDuplicates = function (nums) {
 
     let k = 0;
 
@@ -20,4 +20,28 @@ var removeDuplicates = function(nums) {
     return k;
 };
 
-console.log(removeDuplicates([1, 1, 2]));
+// console.log(removeDuplicates([1, 1, 2]));
+
+
+
+
+
+// Problem-2
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return i;
+        }
+    }
+
+    return -1;
+};
+
+// console.log(search([-1, 0, 3, 5, 9, 12], 9));
