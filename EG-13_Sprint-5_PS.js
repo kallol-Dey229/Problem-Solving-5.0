edf0ca7a1b0bc217aa03dd625e77425795d22766
Kallol Dey <kalloldey067@gmail.com>
2026-09-20 00:09:44 +0600

    problem 3 solved

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -45,3 +45,30 @@ var search = function (nums, target) {
 };
 
 // console.log(search([-1, 0, 3, 5, 9, 12], 9));
+
+
+
+
+//problem-3
+
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return i;
+        } else if (nums[i] > target) {
+            return i;
+        }
+    }
+    return nums.length;
+};
+
+
+
+console.log(searchInsert([1, 3, 5, 6], target = 5));
