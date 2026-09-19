bb738b5ba3b96a5c0337d9538b223e47f401afef
Kallol Dey <kalloldey067@gmail.com>
2026-09-19 23:04:21 +0600

    problem 1 solved

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -1,11 +1,23 @@
+// problem-1
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {boolean}
  */
-var checkSubarraySum = function(nums, k) {
-    // your code goes here
+var removeDuplicates = function(nums) {
+
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (i === 0 || nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
 };
 
-
-console.log(checkSubarraySum([1, 1, 2]));
+console.log(removeDuplicates([1, 1, 2]));
