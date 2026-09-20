b6e3f7a59d6fd960ca494b43dee10c5809e2e7a8
Kallol Dey <kalloldey067@gmail.com>
2026-09-20 14:22:11 +0600

    problem 9 solved

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -192,9 +192,9 @@ var rotate = function(nums, k) {
 
 // problem-8
 
-
 var MinStack = function() {
     this.stack = [];
+    this.minStack = [];
 };
 
 /**
@@ -202,14 +202,24 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(val) {
+
     this.stack.push(val);
+
+    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+        this.minStack.push(val);
+    }
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-    this.stack.pop();
+
+    let value = this.stack.pop();
+
+    if (value === this.minStack[this.minStack.length - 1]) {
+        this.minStack.pop();
+    }
 };
 
 /**
@@ -223,15 +233,38 @@ MinStack.prototype.top = function() {
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    let min = this.stack[0];
-
-    for (let i = 1; i < this.stack.length; i++) {
-        if (this.stack[i] < min) {
-            min = this.stack[i];
-        }
-    }
-
-    return min;
+    return this.minStack[this.minStack.length - 1];
 };
 
 
+
+
+
+// problem-9
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+
+    for (let i = 0; i < nums.length; i++) {
+
+        let sum = nums[i];
+
+        for (let j = i + 1; j < nums.length; j++) {
+
+            sum = sum + nums[j];
+
+            if (sum % k === 0) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+};
+
+// console.log(checkSubarraySum([23, 2, 4, 6, 7], 6));
